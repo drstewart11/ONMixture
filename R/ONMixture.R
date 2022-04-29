94a9333c2b485c2e6e56b5fba2a3a6c82d91ffcb
David R. Stewart <randy.stewart1166@gmail.com>
2022-04-29 06:04:20 -0600

    Update ONMixture.R

--- R/ONMixture.R
+++ R/ONMixture.R
@@ -228,7 +228,7 @@ countmix<-function(count,mgmt,hab,species=c("YCHUB","BSHINER")){
       y[i,j,k,t]~dbin(q[i,j,k,t],N[i,k,t])
 
       #Detection probabilities
-      q[i,j,k,t]~dbeta(6,9) #Slightly informative prior
+      q[i,j,k,t]~dbeta(9,11) #Slightly informative prior
       #logit(q[i,j,k,t])<-alpha[i,j,k]
       }}}}
 
